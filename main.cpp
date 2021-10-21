3d553439d9863f1ff3f9060b55ca248fe16b6622
jinfei <mal584040934@163.com>
2021-10-21 16:25:50 +0800

    test git name

--- main.cpp
+++ main.cpp
@@ -1,10 +1,12 @@
 #include "./mysql/sql_connection_pool.h"
+#include <iostream>
 
+using namespace std;
 
 int main(){
     mysql_connection_pool msyql;
     // mysql.init();
 
-
+    cout << "hello, this is myserver ..." << endl;
     return 0;
 }
